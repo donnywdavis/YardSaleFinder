7ba5490a0e9cf7bbdf24e529a9bf447593fa54fb
Donny Davis <donnywdavis@icloud.com>
2016-06-09 17:23:22 -0400

    Start on our user model

---
+++ YardSaleFinder/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  YardSaleFinder
+//
+//  Created by Donny Davis on 6/9/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import Foundation
+import Gloss
+import CoreLocation
+
+struct User: Decodable {
+    var uid: String?
+    var name: String?
+    var latitude: Double?
+    var longitude: Double?
+    var yardSales: [String]?
+    let created: NSDate?
+    
+    init?(json: JSON) {
+        uid = "uid" <~~ json
+        name = "name" <~~ json
+        latitude = "latitude" <~~ json
+        longitude = "longitude" <~~ json
+        yardSales = "yardSales" <~~ json
+        created = NSDate.dateFromString("created" <~~ json)
+    }
+}
