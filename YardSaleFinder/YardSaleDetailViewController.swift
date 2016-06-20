35c9f1e087ab6c04a5832b1f71bf61600f97b389
Donny Davis <donnywdavis@icloud.com>
2016-06-20 15:12:52 -0400

    Add fields to the detail view to add an address and select a date with start and end times

--- YardSaleFinder/YardSaleDetailViewController.swift
+++ YardSaleFinder/YardSaleDetailViewController.swift
@@ -8,18 +8,160 @@
 
 import UIKit
 
-class YardSaleDetailViewController: UIViewController {
+class YardSaleDetailTableViewController: UITableViewController {
 
     // MARK: IBOutlets
     
+    @IBOutlet weak var street1TextField: UITextField!
+    @IBOutlet weak var street2TextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var startTimePicker: UIDatePicker!
+    @IBOutlet weak var endTimePicker: UIDatePicker!
+    
     // MARK: Properties
+    
+    var isDatePickerVisible = false
+    var isStartTimePickerVisible = false
+    var isEndTimePickerVisible = false
     
     // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView(frame: CGRectZero)
     }
 
+}
+
+// MARK: Table View Delegates
+
+extension YardSaleDetailTableViewController {
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 1 {
+            toggleDateTimePicker(indexPath.row)
+            if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                updateDateTimeLabel(cell, row: indexPath.row)
+            }
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+}
+
+// MARK: Table View Data Sources
+
+extension YardSaleDetailTableViewController {
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30.0
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(red: 0/255.0, green: 178/255.0, blue: 51/255.0, alpha: 1.0)
+        headerView.tintColor = UIColor.whiteColor()
+        
+        return headerView
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Location"
+            
+        case 1:
+            return "Date/Time"
+            
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (1, 1) where !isDatePickerVisible:
+            return 0
+            
+        case (1, 3) where !isStartTimePickerVisible:
+            return 0
+            
+        case (1, 5) where !isEndTimePickerVisible:
+            return 0
+            
+        default:
+            return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+        }
+    }
+    
+}
+
+// MARK: Date/Time Pickers Functions
+
+extension YardSaleDetailTableViewController {
+    
+    func toggleDateTimePicker(row: Int) {
+        switch row {
+        case 0:
+            isDatePickerVisible = !isDatePickerVisible
+            isStartTimePickerVisible = false
+            isEndTimePickerVisible = false
+            
+        case 2:
+            isStartTimePickerVisible = !isStartTimePickerVisible
+            isDatePickerVisible = false
+            isEndTimePickerVisible = false
+            
+        case 4:
+            isEndTimePickerVisible = !isEndTimePickerVisible
+            isDatePickerVisible = false
+            isStartTimePickerVisible = false
+            
+        default:
+            return
+        }
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    func updateDateTimeLabel(cell: UITableViewCell, row: Int) {
+        switch row {
+        case 0:
+            cell.detailTextLabel?.text = NSDateFormatter.localizedStringFromDate(datePicker.date, dateStyle: .MediumStyle, timeStyle: .NoStyle)
+            
+        case 2:
+            cell.detailTextLabel?.text = NSDateFormatter.localizedStringFromDate(startTimePicker.date, dateStyle: .NoStyle, timeStyle: .ShortStyle)
+            
+        case 4:
+            cell.detailTextLabel?.text = NSDateFormatter.localizedStringFromDate(endTimePicker.date, dateStyle: .NoStyle, timeStyle: .ShortStyle)
+            
+        default:
+            return
+        }
+    }
+    
+    @IBAction func dateSelection(sender: UIDatePicker) {
+        if let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 1)) {
+            updateDateTimeLabel(cell, row: 1)
+        }
+    }
+    
+    @IBAction func startTimeSelection(sender: UIDatePicker) {
+        if let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 3, inSection: 1)) {
+            updateDateTimeLabel(cell, row: 3)
+        }
+    }
+    
+    @IBAction func endTimeSelection(sender: UIDatePicker) {
+        if let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 5, inSection: 1)) {
+            updateDateTimeLabel(cell, row: 5)
+        }
+    }
+    
 }
